2f4ebe1f810f4fa46cafe5b29c10dc854e7325fc
Tony Jin <tonyjin23@gmail.com>
2019-06-05 15:42:01 -0700

    create frogs (quantity) - new class renderLoad (lilies) - debugged dynamic create

--- 7_frogriders/assets/js/main.js
+++ 7_frogriders/assets/js/main.js
@@ -2,20 +2,35 @@ $(document).ready(startApp);
 
 
 function startApp() {
+    var gameBoard = new renderLoad();
+    gameBoard.gameLoad(9);
     var frogLocation = new frog('.gameSquare');
+    var createFrogs = new frog();
+    createFrogs.createFrogs(2);
     
 }
-
+class renderLoad {
+    constructor() {
+        this.lily = null;
+    }
+    gameLoad(lilyCount) {
+            this.lily = $("<div>").attr('class', 'lilyPad');
+            $('.gameSquare').append(this.lily);
+        }
+}
 class frog {
     constructor(targetSquare) {
-       this.indexLocation = $(targetSquare); 
-    
+        this.indexLocation = $(targetSquare); 
+        this.makeFrog = null;
     }   
     addEventListeners() {
         this.indexLocation.click(this.frogIndex);
     }
-    createFrogs() { //tony
-
+    createFrogs(frogCount) { //tony
+        for (var i = 0; i <= frogCount; i++) {
+            this.makeFrog = $('<div>').addClass('frogImg');
+            $('.' + i).append(this.makeFrog);
+        }
     }
     frogIndex() { //dwight
 
