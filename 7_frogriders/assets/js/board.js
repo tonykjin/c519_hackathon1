ac2d22c8f406833abe236ac8f1e774133aeedd40
Dwight Millar <millar.dwight@gmail.com>
2019-06-06 10:54:57 -0700

    working on removing frogs - incomplete

--- 7_frogriders/assets/js/board.js
+++ 7_frogriders/assets/js/board.js
@@ -1,15 +1,20 @@
 class Gameboard {
+    newFrog = new Frog();
+
     constructor() {
+
         this.w = null;
         this.v = null;
         this.moveIndex = this.moveIndex.bind(this);
-        this.removeFrogs = this.removeFrogs.bind(this);
     }
     addEventListeners() {
-        $('.gameSquare').click(this.gameArea.removeFrogs($(event.currentTarget).attr('id'), $));
-        var y = $(this).attr('id').split('-')[0];
-        var x = $(this).attr('id').split('-')[1];
-        return [y,x];
+        this.removeFrogs = this.removeFrogs.bind(this);
+
+        $('.gameSquare').click(function(event) {
+            debugger;
+
+            this.removeFrogs($(event.currentTarget).attr('id'));
+        });
     }
     frogIndex() { 
         this.frogArray = [[1,1,1],
@@ -20,7 +25,7 @@ class Gameboard {
             for (var frogArrayIndexInner = 0; frogArrayIndexInner < this.frogArray[frogArrayIndexOuter].length; frogArrayIndexInner++){
                 createFrogsIndex++;
                 if (this.frogArray[frogArrayIndexOuter][frogArrayIndexInner] === 1){
-                    this.createFrogs(createFrogsIndex);
+                    this.newFrog.createFrogs;
                     //display frog
                 }else{
                     //don't
@@ -52,12 +57,16 @@ class Gameboard {
             }
         }
     }
-    removeFrogs(y,x) {
+    removeFrogs(id) {
         //square of frog to move = [w][v]
        //square to move selected to = [i][x]
        //square of frog to remove = [(w+x)/2][(v+y)/2]
        //use .splice to change array[(w+x)/2][(v+y)/2] from 1 to 0
        //add to points by 1
+
+        var y = id.split('-')[0];
+        var x = id.split('-')[1];
+
         var w = this.moveIndex(y,x)[0];
         var v = this.moveIndex(y,x)[1];
         var x = this.moveIndex(y,x)[2];
